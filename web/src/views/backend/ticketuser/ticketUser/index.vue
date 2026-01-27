24989b5fa90620d01c5e4d830dfa362972d14192
Kaixin <2548880572@qq.com>
2026-01-27 18:07:13 +0800

    提交代码

--- web/src/views/backend/ticketuser/ticketUser/index.vue
+++ web/src/views/backend/ticketuser/ticketUser/index.vue
@@ -96,7 +96,7 @@ const baTable = new baTableClass(
             { label: t('ticketuser.ticketUser.email'), prop: 'email', align: 'center', operator: 'eq', sortable: 'custom', width: 160 },
             { label: t('ticketuser.ticketUser.created_at'), prop: 'created_at', align: 'center', operator: 'eq', sortable: 'custom', width: 160 },
             { label: t('ticketuser.ticketUser.updated_at'), prop: 'updated_at', align: 'center', operator: 'eq', sortable: 'custom', width: 160 },
-            { label: t('ticketuser.ticketUser.deleted_at'), prop: 'deleted_at', align: 'center', operator: 'eq', sortable: 'custom', width: 160 },
+            // { label: t('ticketuser.ticketUser.deleted_at'), prop: 'deleted_at', align: 'center', operator: 'eq', sortable: 'custom', width: 160 },
             { label: t('Operate'), align: 'center', width: 100, render: 'buttons', buttons: optButtons, operator: false },
         ],
         dblClickNotEditColumn: [undefined],
